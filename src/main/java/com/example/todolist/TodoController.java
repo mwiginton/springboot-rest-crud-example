19e822361b9df8af57c4b05fc11bd23d1a1cfd4e
Michelle Wiginton <michelle.wiginton00@gmail.com>
2023-08-15 09:43:57 -0500

    add standardized api response to all endpoints

--- src/main/java/com/example/todolist/TodoController.java
+++ src/main/java/com/example/todolist/TodoController.java
@@ -3,6 +3,7 @@ package com.example.todolist;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,41 +25,69 @@ public class TodoController {
     }
 
     @GetMapping
-    public List<Todo> getAllTodos() {
-        return todoRepository.findAll();
+    public ResponseEntity<ApiResponse> getAllTodos() {
+        try {
+            List<Todo> todos = todoRepository.findAll();
+            return ResponseEntity.ok(new ApiResponse(true, "Todos retrieved successfully", todos));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                 .body(new ApiResponse(false, "Error retrieving todos", null));
+        }
     }
     
     @GetMapping("/{id}")
-    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
-        Todo todo = todoRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
-        
-        return ResponseEntity.ok(todo);
+    public ResponseEntity<ApiResponse> getTodoById(@PathVariable Long id) {
+        try {
+            Todo todo = todoRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
+
+            return ResponseEntity.ok(new ApiResponse(true, "Todo retrieved successfully", todo));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                 .body(new ApiResponse(false, "Error retrieving todo with id: " + id, null));
+        }
     }
 
     @PostMapping
-    public Todo createTodo(@RequestBody Todo todo) {
-        return todoRepository.save(todo);
+    public ResponseEntity<ApiResponse> createTodo(@RequestBody Todo todo) {
+        try {
+            Todo createdTodo = todoRepository.save(todo);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                                 .body(new ApiResponse(true, "Todo created successfully", createdTodo));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                 .body(new ApiResponse(false, "Error creating todo", null));
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @RequestBody Todo todoDetails) {
-        Todo todo = todoRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
-        
-        todo.setTitle(todoDetails.getTitle());
-        todo.setCompleted(todoDetails.isCompleted());
+    public ResponseEntity<ApiResponse> updateTodo(@PathVariable Long id, @RequestBody Todo todoDetails) {
+        try {
+            Todo todo = todoRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
 
-        Todo updatedTodo = todoRepository.save(todo);
-        return ResponseEntity.ok(updatedTodo);
+            todo.setTitle(todoDetails.getTitle());
+            todo.setCompleted(todoDetails.isCompleted());
+
+            Todo updatedTodo = todoRepository.save(todo);
+            return ResponseEntity.ok(new ApiResponse(true, "Todo updated successfully", updatedTodo));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                 .body(new ApiResponse(false, "Error updating todo with id: " + id, null));
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteTodo(@PathVariable Long id) {
-        Todo todo = todoRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
+    public ResponseEntity<ApiResponse> deleteTodo(@PathVariable Long id) {
+        try {
+            Todo todo = todoRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: " + id));
 
-        todoRepository.delete(todo);
-        return ResponseEntity.ok().build();
+            todoRepository.delete(todo);
+            return ResponseEntity.ok(new ApiResponse(true, "Todo deleted successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                 .body(new ApiResponse(false, "Error deleting todo with id: " + id, null));
+        }
     }
 }
